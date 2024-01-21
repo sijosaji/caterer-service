71ca00eb35ec710e884098837221beab69df65ee
asifmons <asifshaji777@gmail.com>
2024-01-21 13:02:04 +0530

    removed unused imports

--- src/main/java/com/vendor/caterer/services/PackageService.java
+++ src/main/java/com/vendor/caterer/services/PackageService.java
@@ -4,7 +4,6 @@ import com.vendor.caterer.dao.PackageRepository;
 import com.vendor.caterer.dto.PackageCreateRequest;
 import com.vendor.caterer.dto.PackageUpdateRequest;
 import com.vendor.caterer.interfaces.PackageMapper;
-import com.vendor.caterer.model.Category;
 import com.vendor.caterer.model.Package;
 import com.vendor.caterer.model.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
